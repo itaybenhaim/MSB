d2bffa2d17d524f3bef2becc839001c05c8a82f5
itaybenhaim <itaybenhaim100@gmail.com>
2016-09-14 16:04:20 +0300

    Add files via upload

---
+++ mypackage/Shape.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+
+
+public abstract class Shape {
+	
+	public abstract String getColor();
+
+	public abstract void setColor(String c);
+	public abstract double getArea();
+	
+	
+}
