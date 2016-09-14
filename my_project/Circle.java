ed798ba1339839d25859273affeccfbd7fe02d7e
itaybenhaim <itaybenhaim100@gmail.com>
2016-09-14 14:51:54 +0300

    Add files via upload

---
+++ my_project/Circle.java
@@ -0,0 +1,55 @@
+/*
+ * interface: 
+ * 
+ */
+
+package my_project;
+
+public class Circle {
+	//constants
+	static final String DEF_COLOR = "black";
+	static final double DEF_RADIUS = 5.0;
+	
+	//fields
+	private String color;
+	private double radius;
+	
+	
+	//methods
+	public void setColor(String color){
+		
+	}
+	public String getColor(){
+		return this.color;
+	}
+	public double getRadius(){
+		return this.radius;
+	}
+	public double getArea(){
+		return Math.PI * radius * radius;
+	}
+	
+	
+	//end of methods
+	
+	
+	//ctors
+	Circle(){
+		this(DEF_RADIUS, DEF_COLOR);
+	}
+	Circle(String color){
+		this(DEF_RADIUS, color);
+	}
+	Circle(double radius){
+		this(radius, DEF_COLOR);
+	}
+	Circle(double radius, String color){
+		this.radius = radius;
+		this.color = color;
+	}
+	//end of ctors
+
+	public static void main(String[] args) {
+		
+	}
+}
